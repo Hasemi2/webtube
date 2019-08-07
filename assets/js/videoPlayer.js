8543efb4e2273105b9f1b0646e17cd4b13fe4d69
Hasemi2 <h171754@gmail.com>
2019-08-07 17:57:21 +0900

    0807

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,43 @@
+const videoContainer = document.getElementById("jsVideoPlayer");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScreenBtn = document.getElementById("jsFullScreen");
+
+function handlePlayClick() {
+    if (videoPlayer.paused) {
+        videoPlayer.play();
+        playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+        videoPlayer.pause();
+        playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+}
+
+function handleVolumeClick() {
+    if (videoPlayer.muted) {
+        videoPlayer.muted = false;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else {
+        videoPlayer.muted = true;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+}
+
+function handleFullScreenClick() {
+    if (videoPlayer.fullscreen) {
+        videoPlayer.requestFullscreen();
+    } else {
+        videoPlayer.exitFullscreen();
+    }
+}
+
+function init() {
+    playBtn.addEventListener("click", handlePlayClick);
+    volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScreenBtn.addEventListener("click", handleFullScreenClick);
+}
+
+if (videoContainer) {
+    init();
+}
