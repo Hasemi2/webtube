1ceac4483827b6194b877ea690a27823b51d9635
Hasemi2 <h171754@gmail.com>
2019-08-11 17:23:05 +0900

    [비디오] Record 후 파일 다운로드 구현

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,67 +2,59 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-let streamObject; //녹화된 object 영상물
+let streamObject;
 let videoRecorder;
 
-const handleVideoData = event => {
-  console.log("data ===> " , event.data);
-
-  const { data: videoFile } = event;
-  console.log("videoFile ===> " , videoFile )
-};
-
-// returns file, that we can send to the server.
-function dataUrlToFile(dataUrl) {
-  var binary = atob(dataUrl.split(',')[1]),
-  data = [];
-
-  for (var i = 0; i < binary.length; i++)
-    data.push(binary.charCodeAt(i));
-
-  return new File([new Uint8Array(data)], 'recorded-video.webm', {
-    type: 'video/webm'
-  });
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData); //레코딩이 끝났을떄 전체 데이터를 얻을 수 있음
+  recordBtn.addEventListener("click", stopRecording);
 }
 
 const stopRecording = () => {
   videoRecorder.stop();
   recordBtn.removeEventListener("click", stopRecording);
   recordBtn.addEventListener("click", getVideo);
-  recordBtn.innerHTML = "Start recording";
-};
+  recordBtn.innerHTML = "Start Recording";
 
-const startRecording = () => {
-  videoRecorder = new MediaRecorder(streamObject);
-  videoRecorder.start();
-  videoRecorder.addEventListener("dataavailable", handleVideoData);
-  recordBtn.addEventListener("click", stopRecording);
-};
+}
+
+const handleVideoData = (event) => {
+  const { data: videoFile } = event;
+  //file download 파일에서부터 url 추출
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm"; //webm 구글에서 지원하느 오픈소스 동영상 포맷
+  document.body.appendChild(link);
+  link.click();
+
+}
 
 const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: { 
-          width: 1280, 
-          height: 720 
-        }
+      auido: true,
+      video: {
+        width: 1280,
+        height: 720
+      },
     });
-
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
-    recordBtn.innerHTML = "Stop recording";
+    recordBtn.innerHTML = "Stop Recording";
     streamObject = stream;
     startRecording();
   } catch (error) {
-    recordBtn.innerHTML = "Cant record";
+    recordBtn.innerHTML = "Can't record ";
+
   } finally {
     recordBtn.removeEventListener("click", getVideo);
   }
-};
+}
 
-function init() {
+const init = () => {
   recordBtn.addEventListener("click", getVideo);
 }
 
