44a18b4e2685816090a7802d7447c7c269c9509d
Hasemi2 <h171754@gmail.com>
2019-08-13 10:54:36 +0900

    [공통] body-parser 모듈 대체 => express 제공 모듈 사용

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -26,8 +25,7 @@ app.set("view engine" , "pug");
 app.use("/uploads" , express.static("uploads")); //정적 파일을 제공하는 미들웨어 함수 uploads의 디렉토리에 포함된 동영상 url을 정적 파일로 제공함
 app.use("/static" , express.static("static")); //webpack build시 generate 되는 static 폴더의 css와 js파일이 들어있는 폴더
 app.use(cookieParser()); //쿠키를 전달받아서 사용할수 있도록 만들어주는 미들웨어
-app.use(bodyParser.urlencoded({extended:true}));  //json 형태의  request 데이터 검사
-app.use(bodyParser.json()); 
+app.use(express.json());  //body parser 모듈 대체
 app.use(morgan("dev")); //logging
 app.use(session({
         secret : process.env.COOKIE_SECRET,
